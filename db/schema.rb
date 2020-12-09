7da32854ab25835e251994ac5feda7d078655e88
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2020-12-09 19:49:01 -0300

    Created StudyItem model and improved viewer

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_12_09_221540) do
+
+  create_table "study_items", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.boolean "status"
+    t.datetime "deadline"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
