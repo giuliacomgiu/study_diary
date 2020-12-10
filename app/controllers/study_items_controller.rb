ec9cfe2b2881a53a2bbd52f9a1dd21c6b984e3e9
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2020-12-10 11:11:54 -0300

    Included study_items resources and removed home. POST requests are currently working

--- app/controllers/study_items_controller.rb
+++ app/controllers/study_items_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class StudyItemsController < ApplicationController
   def index
     @study_items = StudyItem.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'home#index'
+  resources :study_items
+  root to: 'study_items#index'
 end
